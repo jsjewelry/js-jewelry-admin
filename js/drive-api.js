be1842eecf832b56dc3c0cb5855aab580568bca1
chopklintuesin <chopklintuesin@gmail.com>
2026-07-24 10:09:41 +0700

    Add Samsung image decode fallback

--- js/drive-api.js
+++ js/drive-api.js
@@ -63,9 +63,10 @@ function compressImage(file, maxPx = 900, quality = 0.85) {
       return;
     }
 
-    const objectUrl = URL.createObjectURL(file);
+    let objectUrl = URL.createObjectURL(file);
     const img = new Image();
     let settled = false;
+    let usingDataUrlFallback = false;
     const timer = setTimeout(() => {
       fail(new Error('\u0e40\u0e15\u0e23\u0e35\u0e22\u0e21\u0e23\u0e39\u0e1b\u0e19\u0e32\u0e19\u0e40\u0e01\u0e34\u0e19 30 \u0e27\u0e34\u0e19\u0e32\u0e17\u0e35 \u0e01\u0e23\u0e38\u0e13\u0e32\u0e40\u0e25\u0e37\u0e2d\u0e01\u0e23\u0e39\u0e1b\u0e43\u0e2b\u0e21\u0e48'));
     }, 30000);
@@ -74,7 +75,10 @@ function compressImage(file, maxPx = 900, quality = 0.85) {
       clearTimeout(timer);
       img.onload = null;
       img.onerror = null;
-      URL.revokeObjectURL(objectUrl);
+      if (objectUrl) {
+        URL.revokeObjectURL(objectUrl);
+        objectUrl = null;
+      }
     }
 
     function fail(error) {
@@ -122,9 +126,21 @@ function compressImage(file, maxPx = 900, quality = 0.85) {
     };
 
     img.onerror = () => {
+      if (!usingDataUrlFallback) {
+        usingDataUrlFallback = true;
+        if (objectUrl) {
+          URL.revokeObjectURL(objectUrl);
+          objectUrl = null;
+        }
+        fileToBase64(file)
+          .then(dataUrl => {
+            if (!settled) img.src = dataUrl;
+          })
+          .catch(fail);
+        return;
+      }
       fail(new Error('\u0e40\u0e1a\u0e23\u0e32\u0e27\u0e4c\u0e40\u0e0b\u0e2d\u0e23\u0e4c\u0e2d\u0e48\u0e32\u0e19\u0e23\u0e39\u0e1b\u0e19\u0e35\u0e49\u0e44\u0e21\u0e48\u0e44\u0e14\u0e49 \u0e01\u0e23\u0e38\u0e13\u0e32\u0e43\u0e0a\u0e49\u0e44\u0e1f\u0e25\u0e4c JPG \u0e2b\u0e23\u0e37\u0e2d PNG'));
     };
-    img.decoding = 'async';
     img.src = objectUrl;
   });
 }
